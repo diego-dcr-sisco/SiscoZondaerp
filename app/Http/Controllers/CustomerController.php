144862f867adbd7f66c8a3f24fc7611ed9d80669
diego-dcr-sisco <omenplus.ddrc@gmail.com>
2025-12-15 10:53:49 -0600

    Zonda ERP Config 6.7

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -1661,7 +1661,7 @@ class CustomerController extends Controller
         // Optimizar: traer solo los incidentes necesarios
 
         dd($orders->pluck('id')->toArray());
-        $incidents = OrderIncidents::whereIn('order_id', $orders->pluck('id'))
+        $incidents = OrderIncidents::whereIn('order_id', 60000)//$orders->pluck('id'))
             ->where('question_id', $question_id)
             ->whereIn('device_id', $devices->pluck('id'))
             ->select('id', 'device_id', 'order_id', 'answer')
