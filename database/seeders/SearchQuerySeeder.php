582ba5db31b6b353db495723ccc207fa4eb1b6b0
diego-dcr <omenplus.ddrc@gmail.com>
2026-03-31 14:39:02 -0600

    ZondaERP seeder zonda

--- database/seeders/SearchQuerySeeder.php
+++ database/seeders/SearchQuerySeeder.php
@@ -18,7 +18,8 @@ class SearchQuerySeeder extends Seeder
     {
         $customer_id = 330;
         $contract_id = 250;
-        $services_ids = [144, 58];
+        //$services_ids = [144, 58];
+        $service_id = 58;
 
         $customer = \App\Models\Customer::find($customer_id);
 
@@ -27,7 +28,7 @@ class SearchQuerySeeder extends Seeder
             ->get();
 
         echo "------------------- Orders for Customer ID ({$customer->id}): {$customer->name} and Contract ID: {$contract_id} --------------------- \n";
-        foreach ($services_ids as $service_id) {
+        /*foreach ($services_ids as $service_id) {
             $fetch_order_ids = OrderService::where('service_id', $service_id)
                 ->whereIn('order_id', $orders->pluck('id')->toArray())
                 ->pluck('order_id')
@@ -41,6 +42,17 @@ class SearchQuerySeeder extends Seeder
                 $pos = $index + 1;
                 echo " {$pos}) Order ID: {$order->id}, Service ID: {$service_id}, Customer ID: {$order->customer_id}, Status: {$status_name}\n";
             }
-        }
+        }*/ 
+        
+        $fetch_order_ids = OrderService::where('service_id', $service_id)
+            ->whereIn('order_id', $orders->pluck('id')->toArray())
+            ->pluck('order_id')
+            ->toArray();
+
+        // Eliminar ordenes
+        $delete_cant = Order::whereIn('id', $fetch_order_ids)->count();
+        echo "------------------- Deleting Orders for Service ID: {$service_id} --------------------- \n";
+        echo "Total Orders to Delete: {$delete_cant} \n";
+        Order::whereIn('id', $fetch_order_ids)->delete();
     }
 }
