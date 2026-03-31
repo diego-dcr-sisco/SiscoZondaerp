00b1cb8de6e4b63a661aa293151393c08abbd3bc
diego-dcr <omenplus.ddrc@gmail.com>
2026-03-31 14:05:41 -0600

    ZondaERP seeder zonda

---
+++ database/seeders/SearchQuerySeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use App\Models\OrderService;
+use App\Models\Order;
+use Google\Service\AirQuality\Resource\Forecast;
+
+class SearchQuerySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $customer_id = 330;
+        $contract_id = 250;
+        $services_ids = [144, 58];
+
+        foreach ($services_ids as $service_id) {
+            $fetch_order_ids = OrderService::where('customer_id', $customer_id)
+                ->where('contract_id', $contract_id)
+                ->where('service_id', $service_id)
+                ->pluck('order_id')
+                ->toArray();
+
+            $orders = Order::whereIn('id', $fetch_order_ids)->get();
+            echo "------------------- Orders for Service ID: {$service_id} --------------------- \n";
+            foreach ($orders as $order) {
+                $search_query = $order->search_query;
+                $status_name = isset($order->status->name) ? $order->status->name : '-';
+                echo "> Order ID: {$order->id}, Status: {$status_name}\n";
+            }
+        }
+    }
+}
