98540d26e6932023cb414c9d86382d633a6b2b4e
diego-dcr <omenplus.ddrc@gmail.com>
2026-04-11 12:40:58 -0600

    Cambios para la columna de acciones de seguimientos

--- resources/views/tracking/edit.blade.php
+++ resources/views/tracking/edit.blade.php
@@ -20,13 +20,19 @@
                         <div class="row">
                             <div class="col-lg-6 col-12 mb-3">
                                 <label for="customer" class="form-label">Cliente</label>
-                                <input type="text" class="form-control" value="{{ $tracking->trackable->name }}"
-                                    disabled>
+                                <div class="input-group">
+                                    <span class="input-group-text"><i class="bi bi-person-circle"></i></span>
+                                    <input type="text" class="form-control" value="{{ $tracking->trackable->name }}"
+                                        disabled>
+                                </div>
                                 <input type="hidden" name="trackable_id" value="{{ $tracking->trackable->id }}">
                             </div>
                             <div class="col-lg-6 col-12 mb-3">
                                 <label for="service" class="form-label">Servicio</label>
-                                <input type="text" class="form-control" value="{{ $tracking->service->name }}" disabled>
+                                <div class="input-group">
+                                    <span class="input-group-text"><i class="bi bi-gear-fill"></i></span>
+                                    <input type="text" class="form-control" value="{{ $tracking->service->name }}" disabled>
+                                </div>
                                 <input type="hidden" name="service_id" value="{{ $tracking->service_id }}">
                             </div>
                             @php
@@ -35,26 +41,35 @@
                             @endphp
                             <div class="col-lg-6 col-12 mb-3">
                                 <label for="frequency" class="form-label">Frecuencia</label>
-                                <input type="text" class="form-control" value="{{ $r_type }}" disabled>
+                                <div class="input-group">
+                                    <span class="input-group-text"><i class="bi bi-hourglass-split"></i></span>
+                                    <input type="text" class="form-control" value="{{ $r_type }}" disabled>
+                                </div>
                             </div>
 
                             <div class="col-lg-6 col-3 mb-3">
                                 <label for="next-date" class="form-label is-required">Fecha del seguimiento</label>
-                                <input type="date" class="form-control" id="next-date" name="next_date"
-                                    value="{{ $tracking->next_date }}" required>
+                                <div class="input-group">
+                                    <span class="input-group-text"><i class="bi bi-calendar-event"></i></span>
+                                    <input type="date" class="form-control" id="next-date" name="next_date"
+                                        value="{{ $tracking->next_date }}" required>
+                                </div>
                             </div>
                             <div class="col-lg-6 col-12 mb-3">
                                 <label for="status" class="form-label is-required">Estado</label>
-                                <select class="form-select" id="status" name="status" required>
-                                    <option value="active" {{ $tracking->status == 'active' ? 'selected' : '' }}>Activo
-                                    </option>
-                                    <option value="completed" {{ $tracking->status == 'completed' ? 'selected' : '' }}>
-                                        Completado
-                                    </option>
-                                    <option value="canceled" {{ $tracking->status == 'canceled' ? 'selected' : '' }}>
-                                        Cancelado
-                                    </option>
-                                </select>
+                                <div class="input-group">
+                                    <span class="input-group-text"><i class="bi bi-tag-fill"></i></span>
+                                    <select class="form-select" id="status" name="status" required>
+                                        <option value="active" {{ $tracking->status == 'active' ? 'selected' : '' }}>Activo
+                                        </option>
+                                        <option value="completed" {{ $tracking->status == 'completed' ? 'selected' : '' }}>
+                                            Completado
+                                        </option>
+                                        <option value="canceled" {{ $tracking->status == 'canceled' ? 'selected' : '' }}>
+                                            Cancelado
+                                        </option>
+                                    </select>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -63,20 +78,29 @@
                     <div class="border rounded shadow p-3 mb-3">
                         <div class="mb-3">
                             <label for="title" class="form-label">Título</label>
-                            <input type="text" class="form-control" id="title" name="title"
-                                value="{{ $tracking->title }}" placeholder="Título del seguimiento">
+                            <div class="input-group">
+                                <span class="input-group-text"><i class="bi bi-pencil-square"></i></span>
+                                <input type="text" class="form-control" id="title" name="title"
+                                    value="{{ $tracking->title }}" placeholder="Título del seguimiento">
+                            </div>
                         </div>
 
                         <div class="mb-3">
                             <label for="cost" class="form-label">Costo</label>
-                            <input type="number" step="0.01" min="0" class="form-control" id="cost"
-                                name="cost" value="{{ $tracking->cost }}" placeholder="Costo del seguimiento">
+                            <div class="input-group">
+                                <span class="input-group-text"><i class="bi bi-cash-coin"></i></span>
+                                <input type="number" step="0.01" min="0" class="form-control" id="cost"
+                                    name="cost" value="{{ $tracking->cost }}" placeholder="Costo del seguimiento">
+                            </div>
                         </div>
 
                         <div class="mb-3">
                             <label for="description" class="form-label">Descripción</label>
-                            <textarea class="form-control" id="description" name="description" rows="5"
-                                placeholder="Descripción del seguimiento">{{ $tracking->description }}</textarea>
+                            <div class="input-group">
+                                <span class="input-group-text"><i class="bi bi-chat-left-text"></i></span>
+                                <textarea class="form-control" id="description" name="description" rows="5"
+                                    placeholder="Descripción del seguimiento">{{ $tracking->description }}</textarea>
+                            </div>
                         </div>
                     </div>
                 </div>
