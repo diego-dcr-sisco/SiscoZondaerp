2f169229e4491eccaf97aec3afec5b6b991bac66
diego-dcr <omenplus.ddrc@gmail.com>
2026-04-18 11:04:00 -0600

    Updated areas by seeder in Cepro Culiacan

---
+++ database/seeders/UpdateDevicestoAreasByVersions.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Device;
+
+class UpdateDevicestoAreasByVersions extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $customer_id = 329;
+        $floorplan_id = 49;
+        $version_to_update = 1;
+        $version = 2;
+
+        $devices_to_update = Device::where('floorplan_id', $floorplan_id)
+            ->where('version', $version_to_update)
+            ->get();
+
+        $fetch_devices = Device::where('floorplan_id', $floorplan_id)
+            ->where('version', $version)
+            ->get();
+
+        foreach ($devices_to_update as $device) {
+            $matching_device = $fetch_devices
+                ->where('type_control_point_id', $device->type_control_point_id)
+                ->where('nplan', $device->nplan)
+                ->first();
+
+            if ($matching_device) {
+                $device->update([
+                    'application_area_id' => $matching_device->application_area_id,
+                ]);
+            }
+        }
+    }
+}
