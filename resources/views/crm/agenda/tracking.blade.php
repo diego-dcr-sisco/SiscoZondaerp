da3016045e8d08c3fa74ae871370490aac81a3ca
diego-dcr <omenplus.ddrc@gmail.com>
2026-04-28 11:55:29 -0600

    Export Dailytrackings - Check 9

--- resources/views/crm/agenda/tracking.blade.php
+++ resources/views/crm/agenda/tracking.blade.php
@@ -263,16 +263,14 @@
                                              class="btn btn-success"
                                              onclick="return confirm('✅ COMPLETAR Seguimiento\n\n¿Deseas completar esta actividad?')"
                                              data-bs-toggle="tooltip" data-bs-placement="top"
-                                             data-bs-custom-class="custom-tooltip" data-bs-title="Completar seguimiento"
-                                             style="font-size: 0.95rem;">
+                                             data-bs-custom-class="custom-tooltip" data-bs-title="Completar seguimiento">
                                              <i class="bi bi-check-lg"></i>
                                          </a>
                                          <a href="{{ route('crm.tracking.cancel', ['id' => $tracking->id]) }}"
                                              class="btn btn-danger"
                                              onclick="return confirm('❌ CANCELAR de Seguimiento\n\n¿Deseas cancelar esta actividad?')"
                                              data-bs-toggle="tooltip" data-bs-placement="top"
-                                             data-bs-custom-class="custom-tooltip" data-bs-title="Cancelar seguimiento"
-                                             style="font-size: 0.95rem;">
+                                             data-bs-custom-class="custom-tooltip" data-bs-title="Cancelar seguimiento">
                                              <i class="bi bi-x-lg"></i>
                                          </a>
                                      @endif
@@ -280,8 +278,7 @@
                                          class="btn btn-outline-danger"
                                          onclick="return confirm('ELIMINAR Seguimiento\n\n¿Deseas eliminar esta actividad?')"
                                          data-bs-toggle="tooltip" data-bs-placement="top"
-                                         data-bs-custom-class="custom-tooltip" data-bs-title="Eliminar seguimiento"
-                                         style="font-size: 0.95rem;">
+                                         data-bs-custom-class="custom-tooltip" data-bs-title="Eliminar seguimiento">
                                          <i class="bi bi-trash-fill"></i>
                                      </a>
                                  </div>
