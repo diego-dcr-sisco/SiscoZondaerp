a298b4ff45e7de705dbf5d79f106a588c24c0735
diego-dcr <omenplus.ddrc@gmail.com>
2026-05-14 11:20:04 -0600

    Zonda ERP - Responsive View USERS 2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -86,7 +86,7 @@ class UserController extends Controller
 
 	public function index(): View
 	{
-		$users = User::whereNot('role_id', 4)->orderBy('name')->paginate($this->size);
+		$users = User::whereNot('role_id', 4)->orderBy('name')->orderBy('status_id')->paginate($this->size);
 		$roles = SimpleRole::where('id', '!=', 4)->get();
 		$wk_depts = WorkDepartment::where('id', '!=', 1)->get();
 		$types = ['Usuario Interno', 'Cliente'];
